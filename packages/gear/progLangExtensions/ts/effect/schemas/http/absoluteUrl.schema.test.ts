55f8f74ed97968edde24f72a142744d8d0a11d5a
aurelienbobenriethpro <aurelien.bobenrieth@gmail.com>
2024-08-16 16:11:56 +0200

    ✨ added absolute url & hostname schemas

---
+++ packages/gear/progLangExtensions/ts/effect/schemas/http/absoluteUrl.schema.test.ts
@@ -0,0 +1,27 @@
+import { decode } from "@effect/schema/Schema";
+import { describe, expect, it } from "@effect/vitest";
+import { Effect } from "effect";
+
+import { AbsoluteUrl } from "./absoluteUrl.schema";
+
+describe("absolute url schema", () => {
+  it.effect("should accept absolute urls", () =>
+    Effect.gen(function* () {
+      const someValidUrl = "https://www.google.com";
+
+      const result = yield* decode(AbsoluteUrl)(someValidUrl);
+
+      expect(result).toBe(someValidUrl);
+    }),
+  );
+
+  it.effect("should reject invalid urls", () =>
+    Effect.gen(function* () {
+      const someInvalidUrl = "www.google.com";
+
+      const result = yield* Effect.flip(decode(AbsoluteUrl)(someInvalidUrl));
+
+      expect(result).toBeInstanceOf(Error);
+    }),
+  );
+});
