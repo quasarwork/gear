f35f49a3fc6e54f0c966a4580131a417a2d6a914
aurelienbobenriethpro <aurelien.bobenrieth@gmail.com>
2024-08-19 13:32:43 +0200

    ✨ added error base

--- packages/gear/api/sharedKernel/application/idGenerators/idGeneration.error.ts
+++ packages/gear/api/sharedKernel/application/idGenerators/idGeneration.error.ts
@@ -1,7 +1,3 @@
-import { Data } from "effect";
+import { ErrorBase } from "#progLangExtensions/ts/effect/errors/errorBase";
 
-export class IdGenerationError extends Data.TaggedError("ID_GENERATION_ERROR")<{
-  message: string;
-  cause?: Error["cause"];
-  metadata?: Record<string, unknown>;
-}> {}
+export class IdGenerationError extends ErrorBase("ID_GENERATION_ERROR") {}
