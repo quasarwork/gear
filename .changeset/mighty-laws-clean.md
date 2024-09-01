b3e01f37082c428c56a25bbbcc313659053672f2
aurelienbobenriethpro <aurelien.bobenrieth@gmail.com>
2024-09-01 19:33:34 +0200

    ✨ added gadget jsonPropertyToObjectOrNullUnsafe

--- .changeset/mighty-laws-clean.md
+++ .changeset/mighty-laws-clean.md
@@ -2,4 +2,4 @@
 "@quasarwork/gear": minor
 ---
 
-removed shared kernel as it is more of a 'common' dir & added new schemas, functions etc...
+- removed shared kernel as it is more of a 'common' dir & added new schemas, functions, layers etc... (transliterator, ids, banking, dates, configs, errors, etc...)
---
+++ packages/gear/common/gadget/jsonProperty.parser.ts
@@ -0,0 +1,74 @@
+export type JsonObject = { [Key in string]: JsonValue } & {
+  [Key in string]?: JsonValue | undefined;
+};
+export type JsonArray = JsonValue[] | readonly JsonValue[];
+export type JsonPrimitive = string | number | boolean | null;
+export type JsonValue = JsonPrimitive | JsonObject | JsonArray;
+
+/**
+ * Gadget allows use to configure a model property (PGSQL database) as a JSON property.
+ *
+ * However, it does not support yet configuring a JSON Schema to have a type-safe output when using the
+ * automatically generated JS client.
+ *
+ * Unfortunately, the current output of JSON properties is very unsafe. This utility function aims
+ * to provide a "safer" output.
+ *
+ * @param value - the value to convert
+ * @returns the converted value
+ *
+ * @example
+ * ```ts
+ * const value = jsonPropertyToObjectOrNullUnsafe({
+ *   string: "foo",
+ * });
+ * console.log(value); // { string: "foo" }
+ * ```
+ *
+ * @example
+ * ```ts
+ * const value = jsonPropertyToObjectOrNullUnsafe(1000);
+ * console.log(value); // { number: 1000 }
+ * ```
+ *
+ * @example
+ * ```ts
+ * const value = jsonPropertyToObjectOrNullUnsafe("someNonJsonString");
+ * console.log(value); // { string: "someNonJsonString" }
+ * ```
+ *
+ * @example
+ * ```ts
+ * const value = jsonPropertyToObjectOrNullUnsafe([1, 2, 3]);
+ * console.log(value); // [1, 2, 3]
+ * ```
+ */
+export const jsonPropertyToObjectOrNullUnsafe = (
+  value: JsonValue | null,
+): JsonObject | JsonArray | null => {
+  if (value === null) {
+    return null;
+  }
+
+  if (typeof value === "string") {
+    try {
+      return JSON.parse(value) as JsonObject | JsonArray;
+    } catch {
+      return { string: value };
+    }
+  }
+
+  if (typeof value === "number") {
+    return { number: value };
+  }
+
+  if (typeof value === "boolean") {
+    return { boolean: value };
+  }
+
+  if (typeof value === "object") {
+    return value;
+  }
+
+  return {};
+};
