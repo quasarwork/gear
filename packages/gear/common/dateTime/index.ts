279535430ac6df78dbbb66ebafb0b9ca0a540552
Aurélien Bobenrieth <37140583+aurelienbobenrieth@users.noreply.github.com>
2024-09-02 21:37:52 +0200

    Merge pull request #38
    * 🐛 fixed safe date time exports

--- packages/gear/common/dateTime/index.ts
+++ packages/gear/common/dateTime/index.ts
@@ -4,3 +4,9 @@
 export * from "./safeDateTime.schema.js";
 export * from "./safeDateTime.provider.js";
 export * from "./safeDateTime.provider.error.js";
+
+/**
+ * @since 0.5.1
+ */
+export * from "./__testing__/safeDateTime.provider.deterministic.js";
+export * from "./native/safeDateTime.provider.native.js";
